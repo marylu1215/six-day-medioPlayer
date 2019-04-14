e4371f1aa21372f79f5a6f1ce4aad1d6ef15cf17
jun xie <iumnyo@163.com>
2019-04-14 15:52:53 +0800

    Initial Commit

---
+++ six-day-medioPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  six-day-medioPlayer
+//
+//  Created by MacBook Pro on 2019/4/14.
+//  Copyright © 2019年 MacBookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
